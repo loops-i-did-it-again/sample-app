f85ef8d3b909ef7ca8d14499547685ee7b7438e7
dani <dani.zaghian@gmail.com>
2020-09-07 11:24:15 -0700

    adds api routes with a view

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,7 +1,14 @@
 class Api::ExamplePagesController < ApplicationController
 
   def hello_action
-    render json: {message: "Hello World!"}
+    @message = "Hello World, from an instance variable"
+    @time = Time.now.strftime("%b %e, %l:%M %p")
+    render "hello.json.jb"
+  end
+
+  def goodbye_action
+    # rendering of a web response
+    render json: ["Goodbye", "Farewell"]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   namespace :api do
+    # /api/hello_path => /api/example_pages
     get "/hello_path" => "example_pages#hello_action"
+    get "/goodbye_path" => "example_pages#goodbye_action"
   end
 
 end
